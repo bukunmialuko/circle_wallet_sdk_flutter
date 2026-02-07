dbb3245047db392904f3007501c5ba226fbc4c88
bukunmialuko <oluwabukunmi.aluko@gmail.com>
2026-02-07 01:38:54 +0000

    chore: update android sdk

--- circle_wallet_android/android/settings.gradle.kts
+++ circle_wallet_android/android/settings.gradle.kts
@@ -8,35 +8,6 @@ pluginManagement {
     }
 }
 
-dependencyResolutionManagement {
-
-    repositories {
-        google()
-        mavenCentral()
-
-        // Circle Programmable Wallet SDK repository
-        val localPropertiesFile = File(rootDir, "local.properties")
-
-        if (localPropertiesFile.exists()) {
-            val properties = java.util.Properties()
-            properties.load(localPropertiesFile.inputStream())
-
-            val mavenUrl = properties.getProperty("pwsdk.maven.url")
-            val mavenUsername = properties.getProperty("pwsdk.maven.username")
-            val mavenPassword = properties.getProperty("pwsdk.maven.password")
-
-            if (!mavenUrl.isNullOrEmpty()) {
-                maven {
-                    url = uri(mavenUrl)
-                    credentials {
-                        username = mavenUsername ?: ""
-                        password = mavenPassword ?: ""
-                    }
-                }
-            }
-        }
-    }
-}
 
 plugins {
     id("com.android.library") version "8.12.0" apply false
